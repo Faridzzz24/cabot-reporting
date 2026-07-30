f5ca5553251e9f0ca83f84a14d1d41e085191b02
Faridzzz24 <faridmaulana2495@gmail.com>
2026-07-30 09:25:41 +0700

    feat: replace default laravel favicon with cabot logo

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Sistem Pelaporan Insiden K3 PT Cabot Indonesia - Laporkan kejadian keselamatan kerja dengan cepat dan mudah.">
     <title>@yield('title', 'Pelaporan Insiden K3') - PT Cabot Indonesia</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/cabot-logo.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
